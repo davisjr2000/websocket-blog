1a319ac262066be8d45288b17e4b625abdcfeb64
Davis JR <davisjr2000@hotmail.com>
2019-12-31 20:14:48 -0300

    Implement and Test Hiding Edit/Delete Link for non-owners/non-logged users

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,17 +3,43 @@ require 'rails_helper'
 RSpec.feature "Showing an Article" do
     
     before do
-        john = User.create(email: 'john@mail.com', password: 'password')
-        login_as(john)
-        @article = Article.create(title: "Simple Article", body: 'Lorem Ipsum dolor sit amet', user: john)
+        @john = User.create(email: 'john@mail.com', password: 'password')
+        @greg = User.create(email: 'greg@mail.com', password: 'password')      
+        @article = Article.create(title: "Simple Article", body: 'Lorem Ipsum dolor sit amet', user: @john)
     end
 
-    scenario "A User goes to article page" do
+    scenario "A owner signed in can see both links" do
+        login_as(@john)
         visit "/"
         click_link @article.title
 
         expect(page).to have_content(@article.title)
         expect(page).to have_content(@article.body)
+        expect(page).to have_link("Edit Article")
+        expect(page).to have_link("Delete Article")
+        expect(current_path).to eq(article_path(@article))
+    end
+
+    scenario "A non-owner signed in cannot see both links" do
+        login_as(@greg)
+        visit "/"
+        click_link @article.title
+
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
+        expect(current_path).to eq(article_path(@article))
+    end
+
+    scenario "to a non-signed in user hides Edit/Delete Links" do
+        visit "/"
+        click_link @article.title
+
+        expect(page).to have_content(@article.title)
+        expect(page).to have_content(@article.body)
+        expect(page).not_to have_link("Edit Article")
+        expect(page).not_to have_link("Delete Article")
         expect(current_path).to eq(article_path(@article))
     end
 
