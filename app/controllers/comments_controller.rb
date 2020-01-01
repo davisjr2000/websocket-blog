d156a0be93a2070b888ce94e4248dbe52d28445d
Davis JR <davisjr2000@hotmail.com>
2020-01-01 19:30:28 -0300

    Implement Sign In for Posting Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,10 @@ class CommentsController < ApplicationController
     before_action :set_article
 
     def create
+        unless current_user
+            flash[:alert] = "Please sign in or sign up first"
+            redirect_to new_user_session_path 
+        end
         @comment = @article.comments.build(comment_params) 
         @comment.user = current_user
         if @comment.save
