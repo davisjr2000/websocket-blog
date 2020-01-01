c1af5016b29d9dd84db9a9d270765a2c4927372e
Davis JR <davisjr2000@hotmail.com>
2020-01-01 20:53:48 -0300

    Action Cable works but coffee script is fucking stupid

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,31 @@
 class CommentsController < ApplicationController
     before_action :set_article
-
+    
     def create
-        unless current_user
-            flash[:alert] = "Please sign in or sign up first"
-            redirect_to new_user_session_path 
-        end
-        @comment = @article.comments.build(comment_params) 
+      unless current_user
+        flash[:alert] = "Please sign in or sign up first"
+        redirect_to new_user_session_path
+      else
+        @comment = @article.comments.build(comment_params)
         @comment.user = current_user
+        
         if @comment.save
-            flash[:notice] = "Comment has been created"
+            puts 'Saved Comment!'
+          ActionCable.server.broadcast "comments",
+            render(partial: 'comments/comment', object: @comment)
+          flash[:notice] = "Comment has been created"
         else
-            flash.now[:alert] = "Comment has not been created"
+          flash.now[:alert] = "Comment has not been created"
         end
-        redirect_to article_path(@article)     
+      end
     end
-
+    
     private
-
-    def comment_params
+      def comment_params
         params.require(:comment).permit(:body)
-    end
-
-    def set_article
+      end
+      
+      def set_article
         @article = Article.find(params[:article_id])
-    end
-end
+      end
+  end
